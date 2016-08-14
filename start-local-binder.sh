065c5b8ab10797ac7e10de951e34e951019ea1ed
Nathan Daly <NHDaly@gmail.com>
2016-08-14 13:58:11 -0500

    upgraded TensorFlow v0.7 --> v0.10 && added prettytensor.
    Also changed port on start-local-binder.sh

--- start-local-binder.sh
+++ start-local-binder.sh
@@ -1,8 +1,9 @@
 
 # Echo the IP address + port of the docker instance to connect to:
-echo "----- Jupyter Notebook hosted at: http://`docker-machine ip default`:8888 -----"
+echo "----- Jupyter Notebook hosted at: http://`docker-machine ip default`:8800 -----"
 echo "----- FOR GUI, don't forget to RUN THIS COMMAND in Xterm: -----"
 echo "socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\""
+echo ""
 
 DOCKER_VM_DISPLAY_IP=`ifconfig | pcregrep -M "vboxnet0.*\n.*\n.*" | pcregrep -o "[1-9]+\.[1-9]+\.[1-9]+\.[1-9]+" | head -1`
 
@@ -14,8 +15,8 @@ DOCKER_VM_DISPLAY_IP=`ifconfig | pcregrep -M "vboxnet0.*\n.*\n.*" | pcregrep -o 
 #  - Start the notebook and accept connections from outside the container (0.0.0.0).
 docker run \
   -v `pwd`:/home/main/binder \
-  -it -p 8888:8888 \
+  -it -p 8800:8800 \
   -e DISPLAY="$DOCKER_VM_DISPLAY_IP:0" \
   mybinder \
-  ./start-notebook.sh "--ip=0.0.0.0"
+  ./start-notebook.sh "--port=8800 --ip=0.0.0.0"
 
